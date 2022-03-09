95d8e52204cdd44148f2edd32b7c6b8a4abdbcbc
mcarrillo62 <mcarrillo62@gmail.com>
2022-03-08 20:44:50 -0500

    primera versión

---
+++ app.js
@@ -0,0 +1,30 @@
+addEventListener('DOMContentLoaded', () => {
+    const btn_menu = document.querySelector('.btn_menu')
+    if (btn_menu) {
+        btn_menu.addEventListener('click', () =>{
+            const top_menu_items = document.querySelector('.top_menu_items')
+            top_menu_items.classList.toggle('show')
+            
+        })
+    }
+})
+addEventListener('DOMContentLoaded', () => {
+    const btn_menu = document.querySelector('.btn_menu')
+    if (btn_menu) {
+        btn_menu.addEventListener('click', () =>{
+            const body = document.querySelector('body')
+            body.classList.toggle('stop_scroll')
+            
+        })
+    }
+})
+addEventListener('DOMContentLoaded', () => {
+    const btn_menu = document.querySelector('.btn_menu')
+    if (btn_menu) {
+        btn_menu.addEventListener('click', () =>{
+            const html = document.querySelector('html')
+            html.classList.toggle('stop_scroll')
+            
+        })
+    }
+})
